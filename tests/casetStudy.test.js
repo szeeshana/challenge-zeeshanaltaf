6f6fc766f3dfa6998a38f9556f3834971186973b
zeeshanaltaf <zeeshan@ideawake.com>
2021-03-16 01:45:55 +0500

    updated

--- tests/casetStudy.test.js
+++ tests/casetStudy.test.js
@@ -1,12 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
-
-test('Verify Data Fetch', async (done) => {
-    await request(app).post('/case-study').send({
-        "startDate": "2016-12-30",
-        "endDate": "2018-12-30",
-        "minCount": 4000,
-        "maxCount": 5002
-    }).expect(200)
-    done()
-});
+describe('Case Study', () => {
+    test('Verify Data Fetch', async (done) => {
+        await request(app).post('/case-study').send({
+            "startDate": "2016-12-30",
+            "endDate": "2018-12-30",
+            "minCount": 4000,
+            "maxCount": 5002
+        }).expect(200).then((response) => {
+            // Check type and length
+            expect(Array.isArray(response.body.records)).toBeTruthy();
+            expect(response.body.records.length).toEqual(1);
+            done()
+        });
+    }, 30000);
+})
